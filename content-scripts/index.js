68d39598401e4cd53d8abcad55ee988c7f5e53bb
Nicolas Bourret <nicolas.bourret48@gmail.com>
2023-08-14 15:27:15 +0200

    remove border on selection

--- content-scripts/index.js
+++ content-scripts/index.js
@@ -1,20 +1,27 @@
 const elementsOvered = [];
 
+const selectElement = (element) => {
+  element.addEventListener("click", () => {
+    element.style.border = "none";
+    document.removeEventListener("mouseover", addAndRemoveBorder);
+  });
+};
+
+const addAndRemoveBorder = (e) => {
+  if (elementsOvered.length > 0) {
+    elementsOvered[0].style.border = "none";
+    elementsOvered.shift();
+  }
+  const element = e.target;
+  element.style.border = "1px solid green";
+  elementsOvered.push(element);
+
+  selectElement(element);
+};
+
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
   if (req.activateSelection) {
-    document.addEventListener("mouseover", (e) => {
-      if (elementsOvered.length > 0) {
-        elementsOvered[0].style.border = "none";
-        elementsOvered.shift();
-      }
-      const element = e.target;
-      element.style.border = "1px solid green";
-      elementsOvered.push(element);
-
-      element.addEventListener("click", (e) => {
-        console.log(e.target);
-      });
-    });
+    document.addEventListener("mouseover", addAndRemoveBorder);
     sendResponse({ status: "activate" });
   } else {
     sendResponse({ status: "not-activate" });
